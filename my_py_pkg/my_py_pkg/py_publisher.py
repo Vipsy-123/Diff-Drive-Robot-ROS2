1b18c85174c084cbc98ed6f6f95623221e368e36
Vipsy-123 <pardeshivipul18@gmail.com>
2024-06-01 02:53:06 +0530

    Added Robot state Parameter in Python Publisher file

--- my_py_pkg/my_py_pkg/py_publisher.py
+++ my_py_pkg/my_py_pkg/py_publisher.py
@@ -7,14 +7,15 @@ from example_interfaces.msg import String
 class RobotStatePublisher(Node):
     def __init__(self):
         super().__init__("robot_state_publisher")
-        self.robot_state = "Good"
+        self.declare_parameter("robo_state","GOOD")
+        self.robot_state = self.get_parameter("robo_state")
         self.publisher = self.create_publisher(String,"robot_state",10)
         self.timer = self.create_timer(0.5,self.timer_callback)
         self.get_logger().info("Hello World")
 
     def timer_callback(self):
         msg = String()
-        msg.data = "Robot state is " + str(self.robot_state) 
+        msg.data = "Robot state is " + str(self.robot_state.value) 
         self.publisher.publish(msg)
 
 def main(args=None):
