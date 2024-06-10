038fe458d0b22aa984e21c9affdc93ed3a13aca5
Vipsy-123 <pardeshivipul18@gmail.com>
2024-06-10 23:14:09 +0530

    Added Following things - Added Camera Links - Added Camera Plugin - Added My World

--- my_robot_description/launch/display.launch.py
+++ my_robot_description/launch/display.launch.py
@@ -7,8 +7,11 @@ from ament_index_python.packages import get_package_share_path
 
 def generate_launch_description():
 
-    urdf_path = os.path.join(get_package_share_path('my_robot_description'),('urdf'),('my_robot.urdf'))
-    rviz_config_path = os.path.join(get_package_share_path('my_robot_description'),('rviz'),('my_robot_config.rviz'))
+    urdf_path = os.path.join(get_package_share_path('my_robot_description'),
+                             'urdf', 'my_robot.urdf.xacro')
+    rviz_config_path = os.path.join(get_package_share_path('my_robot_description'),
+                                    'rviz', 'urdf_config.rviz')
+    
     robot_description = ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
 
     robot_state_publisher_node = Node(
@@ -16,19 +19,20 @@ def generate_launch_description():
         executable="robot_state_publisher",
         parameters=[{'robot_description': robot_description}]
     )
-    joint_state_publisher_node = Node(
+
+    joint_state_publisher_gui_node = Node(
         package="joint_state_publisher_gui",
         executable="joint_state_publisher_gui"
     )
+
     rviz2_node = Node(
         package="rviz2",
         executable="rviz2",
         arguments=['-d', rviz_config_path]
     )
 
-
     return LaunchDescription([
         robot_state_publisher_node,
-        joint_state_publisher_node,
+        joint_state_publisher_gui_node,
         rviz2_node
     ])
